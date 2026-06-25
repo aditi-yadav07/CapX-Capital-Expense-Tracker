781b26d81dd26ba68c02b36e9d1d5169527566ef
Aditi Yadav <missaditi768@gmail.com>
2026-06-25 14:52:01 +0530

    cleaned repository

--- paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyAndRecordPayment } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
-
-// Route: Scanner se payment hone ke baad database me entry karne ke liye
-router.post('/verify-record', protect, verifyAndRecordPayment);
-
-module.exports = router;
