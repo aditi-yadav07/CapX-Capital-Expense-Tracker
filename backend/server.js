495b55352a2440d8e123f80d94b4f3f45f3bcfa3
Aditi Yadav <missaditi768@gmail.com>
2026-07-03 10:55:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,5 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: "Global Server Breakdown", error: err.message });
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
 
 module.exports = app;
