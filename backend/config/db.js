182c34235979d1ac7fc2b4c4cb3a0ecd6ae286b6
Aditi Yadav <missaditi768@gmail.com>
2026-07-07 23:31:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,18 +10,20 @@ dns.lookup(process.env.DB_HOST, (err, address) => {
 });
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT, 
-    ssl: {
-        rejectUnauthorized: false
-    },
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    connectTimeout: 20000 
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
 });
 
 module.exports = pool.promise();
