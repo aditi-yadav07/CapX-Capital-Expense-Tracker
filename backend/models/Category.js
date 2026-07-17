0ae6e989434aa2cf2e87259ba9ab0c0008dc9250
Aditi Yadav <missaditi768@gmail.com>
2026-07-17 22:16:06 +0530

    Update Category.js

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 const Category = {
     // Saari categories nikalna (Taki frontend me dropdown me dikha sakein)
     getAll: async () => {
-        const [rows] = await db.execute('SELECT * FROM Categories');
+        const [rows] = await db.execute('SELECT * FROM categories');
         return rows;
     }
 };
