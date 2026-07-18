179243cf506a4f7f2efc3bbe4aabdd32364f20e4
Aditi Yadav <missaditi768@gmail.com>
2026-07-18 22:16:30 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from "./context/ThemeContext";
 import App from './pages/App.jsx';
 import './index.css'; // Humari neon CSS file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+    </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
